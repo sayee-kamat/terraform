bf13312473b5416789c49b765363cb1e84c94ba1
sayee-kamat <saikamat1912@gmail.com>
2022-12-05 16:40:46 +0530

    Delete backend.tf
    deleted

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-demo-sept-2022"
-    key            = "tfdemo/backend"
-    region         = "ap-south-1"
-    dynamodb_table = "tf-state-lock-demo"
-  }
-}
