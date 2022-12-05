144880c3dc3f6035c2d2561a672e4d2e6f6c9d0d
sayee-kamat <saikamat1912@gmail.com>
2022-12-05 17:58:12 +0530

    Update ec2.tf
    changing type

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2_demo" {
   ami           = "ami-079b5e5b3971bd10d"
-  instance_type = "t2.micro"
+  instance_type = "t2.samll"
   tags = {
     Name = "Demo My EC2 Terraform Instance"
   }
